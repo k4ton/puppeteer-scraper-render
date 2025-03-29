74ea87a39410dc975a21f795d2c5a3c6b11b865d
K1 <stivan_ivanov@yahoo.it>
2025-03-29 16:31:45 +0100

    Dynamic URL Puppeteer Scraper - Changed networkidle2 to domcontentloaded in scrapeLogic

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,5 +1,5 @@
 module.exports = async (page, url) => {
-    await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 }); // Wait for the page to load completely
+    await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 60000 }); // Wait for the page to load completely
     const html = await page.content(); // <--- This grabs full DOM HTML
   
     return {
