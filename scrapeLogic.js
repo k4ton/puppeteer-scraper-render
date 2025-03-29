ae5ca091e2320f288c1f7440d345fd6fce967a02
K1 <stivan_ivanov@yahoo.it>
2025-03-29 16:22:44 +0100

    Dynamic URL Puppeteer Scraper - Added Page Timeout

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,6 +1,5 @@
 module.exports = async (page, url) => {
-    await page.goto(url, { waitUntil: 'networkidle2' });
-  
+    await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 }); // Wait for the page to load completely
     const html = await page.content(); // <--- This grabs full DOM HTML
   
     return {
